dd8522568863331f55a3dc1befca67064b87aa1e
daniel_n <nevilldani@ecs.vuw.ac.nz>
2021-03-16 17:24:22 +1300

    Added the updated studentdb jar fire

--- src/main/java/nz/ac/wgtn/swen301/assignment1/cli/FindStudentDetails.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment1/cli/FindStudentDetails.java
@@ -9,5 +9,7 @@ public class FindStudentDetails {
      * E.g. a user could invoke this by running "java -cp <someclasspath> nz.ac.wgtn.swen301.assignment1.cli.FindStudentDetails id42"
      * @param arg
      */
-    public static void main (String[] arg) {}
+    public static void main (String[] arg) {
+    	
+    }
 }
